efc5867e526ee78f5388551b5dc3e48beca2c9c8
Amir SeerSol <amirgee007@yahoo.com>
2018-08-15 01:56:48 +0500

    approved and not aproved

--- resources/views/lms/admin/registration/parts/table/td/mark_approved.blade.php
+++ resources/views/lms/admin/registration/parts/table/td/mark_approved.blade.php
@@ -4,5 +4,10 @@
         <span class="label label-success">{{ __('lms.words.yes') }}</span>
         <small>{{ __('lms.words.by') }} <span class="text-info js-mark-approved-by">{{ $registration->markApprovedBy->name }}</span></small><br/>
         <small><i class="fa fa-clock-o">&nbsp;{{ date('d M Y h:i a', strtotime($registration->mark_upload_time)) }}</i></small>
+    @else
+
+        <span class="label label-warning">{{ __('lms.words.no') }}</span>
+
     @endif
+
 </td>
