9bf0ba7f45eae426b562a55f5b81b41ebb4183a2
Amir SeerSol <amirgee007@yahoo.com>
2018-08-14 23:55:47 +0500

    mark approved fiedl updated

--- resources/views/lms/admin/teacher/profile.blade.php
+++ resources/views/lms/admin/teacher/profile.blade.php
@@ -58,8 +58,8 @@
                                 <td>{{ $registration->course->year}}</td>
 
                                 <td class="js-td-is-approved">
-                                    @if($registration->is_approved == REGISTRATION_IS_NOT_APPROVED)
-                                        <span class="label label-warning">{{ __('lms.words.not_approved') }}</span>
+                                    @if($registration->mark_approved == REGISTRATION_IS_NOT_APPROVED)
+                                        <span class="label label-warning"><i class="fa fa-remove"></i>{{ __('lms.words.not_approved') }}</span>
                                     @else
                                         <span class="label label-success"><i class="fa fa-check"></i> {{ __('lms.words.approved') }}</span>
                                     @endif
