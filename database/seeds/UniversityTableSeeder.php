9469b2a49dd5624a6bcf82666ba563bded636b2a
Amir SeerSol <amirgee007@yahoo.com>
2018-08-15 23:13:33 +0500

    university table seeder removed

--- database/seeds/UniversityTableSeeder.php
+++ database/seeds/UniversityTableSeeder.php
@@ -14,41 +14,41 @@ class UniversityTableSeeder extends Seeder
      */
     public function run()
     {
-
-        $user = User::find(1);
-
-        $university = new University();
-        $university->name = 'Universidad de Cuenca';
-        $university->email = 'cuenca@test.com';
-        $university->login_user_name = 'Universidad de Cuenca';
-        $university->login_email = 'cuenca@test.com';
-        $university->created_by = 1;
-        $university->updated_by = 1;
-        $university->save();
-        event(new UniversityCreated($university, $university->login_email, $user));
-
-
-        $university = new University();
-        $university->name = 'Plataforma mecapacito';
-        $university->email = 'mecapacito@test.com';
-        $university->login_user_name = 'Plataforma mecapacito';
-        $university->login_email = 'mecapacito@test.com';
-        $university->created_by = 1;
-        $university->updated_by = 1;
-        $university->save();
-        event(new UniversityCreated($university,$university->login_email,  $user));
-
-
-        $university = new University();
-        $university->name = 'Universidad de Guayaquil';
-        $university->email = 'guayaquil@test.com';
-        $university->login_user_name = 'Universidad de Guayaquil';
-        $university->login_email = 'guayaquil@test.com';
-        $university->created_by = 1;
-        $university->updated_by = 1;
-        $university->save();
-
-        event(new UniversityCreated($university,$university->login_email, $user));
+//
+//        $user = User::find(1);
+//
+//        $university = new University();
+//        $university->name = 'Universidad de Cuenca';
+//        $university->email = 'cuenca@test.com';
+//        $university->login_user_name = 'Universidad de Cuenca';
+//        $university->login_email = 'cuenca@test.com';
+//        $university->created_by = 1;
+//        $university->updated_by = 1;
+//        $university->save();
+//        event(new UniversityCreated($university, $university->login_email, $user));
+//
+//
+//        $university = new University();
+//        $university->name = 'Plataforma mecapacito';
+//        $university->email = 'mecapacito@test.com';
+//        $university->login_user_name = 'Plataforma mecapacito';
+//        $university->login_email = 'mecapacito@test.com';
+//        $university->created_by = 1;
+//        $university->updated_by = 1;
+//        $university->save();
+//        event(new UniversityCreated($university,$university->login_email,  $user));
+//
+//
+//        $university = new University();
+//        $university->name = 'Universidad de Guayaquil';
+//        $university->email = 'guayaquil@test.com';
+//        $university->login_user_name = 'Universidad de Guayaquil';
+//        $university->login_email = 'guayaquil@test.com';
+//        $university->created_by = 1;
+//        $university->updated_by = 1;
+//        $university->save();
+//
+//        event(new UniversityCreated($university,$university->login_email, $user));
 
     }
 }
